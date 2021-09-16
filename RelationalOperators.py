54f683ed1f9fb08a4ce48077ae5e931b6d9748a0
John Allen <52216818+JohnBradleyAllen@users.noreply.github.com>
2021-09-16 06:49:49 -0700

    Add files via upload

---
+++ RelationalOperators.py
@@ -0,0 +1,17 @@
+x = 20
+y = 20
+
+# Write the first if/else statement here:
+if x == y :
+  print("These numbers are the same")
+else : 
+  print("These numbers are not the same")
+
+credits = 120
+# Write the second if/else statement here:
+
+if credits >= 120 :
+  print("You have enough credits to graduate!")
+else :
+  print("You do not have enough credits")
+
