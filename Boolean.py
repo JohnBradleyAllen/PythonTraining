95ec36d831016e77d93b1b1ca75f462d960ae5c8
John Allen <52216818+JohnBradleyAllen@users.noreply.github.com>
2021-09-16 06:39:19 -0700

    Add files via upload

---
+++ Boolean.py
@@ -0,0 +1,7 @@
+#his is not a boolean because it is in parethesis and is not capitalized.
+my_baby_bool = "true"
+print(type(my_baby_bool))
+
+#This is a boolean
+my_baby_bool_two = True
+print(type(my_baby_bool_two))
